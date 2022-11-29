64b60b20b003409d92668166928e73975a569800
AbzMijon <egorchiktop228@gmail.com>
2022-11-29 18:15:55 +0300

    i dont know sorry

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios'
 import { useEffect } from "react";
 
 function App() {
+  
+  const [tableData, setTableData] = useState(null);
 
   useEffect(() => {
       axios.get('https://docs.google.com/spreadsheets/d/1UZxoOstzqK6NPKvEROS2X86R5yyINC8gj9ypGERwphQ/edit?usp=sharing').then(response => {
           console.log(response);
+          setTableData(response.data)
       })
   }, [])
 
   return (
     <React.Fragment>
-      
     </React.Fragment>
   );
 }
